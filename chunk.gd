6137d45a1165dd2b706cd259ff74d5f9fdebcf5a
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2024-12-19 14:13:51 -0600

    hi

--- chunk.gd
+++ chunk.gd
@@ -1,16 +1,17 @@
 extends Area2D
 
-var speed = 150.0
+var speed = 10.0
 var death = 1.0
+var dir = Vector2(1, 1);
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass 
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	move_toward(0, 15, delta)
+	self.global_position += (dir * Vector2(speed, speed))
 	death -= delta
-	if death < 0:
+	if death <= 0:
 		queue_free()
 	pass
--- pineapple.gd
+++ pineapple.gd
@@ -4,27 +4,35 @@ var damage    = 10
 var knockback = 64.0
 var anim_life = 1.0
 var expl_time = 1.0
+var wait = 0.0
+var after = false
 
 var chunk = preload("res://chunk.tscn")
 
 func explode():
 	for i in range(16):
 		var t = chunk.instantiate()
-		t.rotate(i*PI/8)
+		t.dir = Vector2()
+		t.rotate(i)
+		t.global_position = self.global_position
 		add_child(t)
 	pass
 
 func _process(delta: float) -> void:
 	expl_time -= delta
-	if expl_time <= 0:
+	if expl_time <= 0.0 &&  not after:
 		explode()
-		queue_free()
-		return
+		$Sprite2D.visible = false
+		wait = 1.0
+		after = true
 	
 	for enemy in get_tree().get_nodes_in_group("Enemy"):
 		if self.overlaps_body(enemy):
 			enemy.take_damage(damage, self)
 			var dist = (enemy.global_position-self.global_position)
 			enemy.inertia = dist.normalized() * knockback
+	wait -= delta
+	if after && wait <= 0.0:
+		queue_free()
 	
 	pass
