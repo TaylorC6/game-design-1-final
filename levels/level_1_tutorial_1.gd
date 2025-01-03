6560c274b4470bf57a339562735db89d722724b2
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2025-01-03 14:35:14 -0600

    1/2 cc

--- levels/level_1_tutorial_1.gd
+++ levels/level_1_tutorial_1.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+@onready var aud_player = $Node2D/AudioStreamPlayer
+@onready var sus = preload("res://Sounds/Suspense_1.mp3")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	aud_player.stream = sus
+	aud_player.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
