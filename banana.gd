4d71f2291980949e648e25378377171325f02df9
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2025-01-07 14:36:42 -0600

    hi

--- banana.gd
+++ banana.gd
@@ -1,33 +1,50 @@
 extends Node2D
 
-var projectile_a = 5
-var projectile_b = 2
+var projectile_a = 500
+var projectile_b = 200
+
+var direction = 0
 
 var first = false
 var second = false
 var s1 = false
 var s2 = false
 var third = false
-var num = 1.0002
+var num = 1.0025
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$AnimatedSprite2D.play_backwards()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Sprite2D.rotate($Sprite2D.rotation + delta)
+	#$Sprite2D.rotate($Sprite2D.rotation + delta)
 	if !first:
-		if (projectile_a < self.position.x):
-			self.position.x = clamp(self.position.x+(delta*10), 0, projectile_a)
+		if (projectile_a > self.position.x):
+			self.position.x = clamp(self.position.x+(delta*100), 0, projectile_a)
 		else:
 			first = true
 	
 	if !second && first:
 		if !s1:
-			if (projectile_b < self.position.y):
-				self.position.x -= num * 5
-				self.position.y += num * 2
+			if (-200 < self.position.y):
+				self.position.y -= pow((num), 2) * 2
+				self.position.x += (num) * 5
+			else: s1 = true
 			num *= num
+			if (num > 3): num = 3
+		if !s2 and s1:
+			if (-300 > self.position.y):
+				self.position.y -= pow(num, .5) * 2
+				self.position.x -= pow((num), 2) * 2
+			else:
+				s2 = true
+				second = true
+			num *= num
+			if (num > 3): num = 3
 	
+	if second and first && !third:
+		print("hi")
+		move_toward(self.position.x, 5000, 5)
+		move_toward(self.position.y, 300, 5)
