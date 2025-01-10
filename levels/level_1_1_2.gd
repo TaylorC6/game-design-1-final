2c704c9db78e13c46d31dcbc5b930529fe8be3bf
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2025-01-10 14:42:22 -0600

    kitchen music 1/10

---
+++ levels/level_1_1_2.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var aud_player = $Node2D/AudioStreamPlayer
+@onready var sus = preload("res://Sounds/Suspense_1.mp3")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	aud_player.stream = sus
+	aud_player.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
