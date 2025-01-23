d90a1513a0e2395758a939ffb17ad8a648e905e4
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2025-01-22 22:16:34 -0600

    hi

--- doo4.gd
+++ doo4.gd
@@ -3,12 +3,12 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$AnimatedSprite2D.play("Desert_Closed")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if (Fpjglobal.ppp["pressure_plate5"] && Fpjglobal.ppp["pressure_plate6"]):
-		$AnimatedSprite2D.play("Stone_Open")
+		$AnimatedSprite2D.play("Desert_Open")
 		await $AnimatedSprite2D.animation_finished
 		queue_free()
